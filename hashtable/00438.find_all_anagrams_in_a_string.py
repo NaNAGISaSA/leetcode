c3632d8c19f71cb72498f1d01f1ea327eabe1132
NaNAGISaSA <cwangzju@126.com>
2020-07-30 16:26:33 +0800

    Finish Exercise 438

--- hashtable/00438.find_all_anagrams_in_a_string.py
+++ hashtable/00438.find_all_anagrams_in_a_string.py
@@ -29,10 +29,77 @@ class Solution2:
                 result.append(index)
         return result
 
-    
+
+"""
+Runtime: 192 ms, faster than 39.03% of Python3 online submissions for Find All Anagrams in a String.
+Memory Usage: 14.7 MB, less than 83.11% of Python3 online submissions for Find All Anagrams in a String.
+"""
 
 
 class Solution3:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        d = {}
+        for char in p:
+            d[char] = d.get(char, 0) + 1
+
+        left, right = 0, 0
+        length_p = len(p)
+        result = []
+        while right < len(s):
+            if s[right] in d:
+                d[s[right]] -= 1
+
+            if right - left == length_p - 1:
+                # print(left, right, d)
+                if set(d.values()) == {0}:
+                    result.append(left)
+                if s[left] in d:
+                    d[s[left]] += 1
+                left += 1
+
+            right += 1
+
+        return result
 
 
+"""
+Runtime: 136 ms, faster than 70.06% of Python3 online submissions for Find All Anagrams in a String.
+Memory Usage: 14.6 MB, less than 97.97% of Python3 online submissions for Find All Anagrams in a String.
+"""
+
+
+class Solution4:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        d = {}
+        for char in p:
+            d[char] = d.get(char, 0) + 1
+
+        left, right = 0, 0
+        length_p = len(p)
+        result = []
+        match = 0
+        match_p = len(set(p))
+        while right < len(s):
+            if s[right] in d:
+                d[s[right]] -= 1
+                if d[s[right]] == 0:
+                    match += 1
+                elif d[s[right]] == -1:
+                    match -= 1
+
+            if right - left == length_p - 1:
+                # print(left, right, match, d)
+                if match == match_p:
+                    result.append(left)
+                if s[left] in d:
+                    d[s[left]] += 1
+                    if d[s[left]] == 0:
+                        match += 1
+                    elif d[s[left]] == 1:
+                        match -= 1
+
+                left += 1
+
+            right += 1
+
+        return result
