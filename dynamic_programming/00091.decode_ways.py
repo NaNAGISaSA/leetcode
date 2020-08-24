2c1af5cab911d1e6e37a9e783d4425d033a3f1da
NaNAGISaSA <cwangzju@126.com>
2020-08-24 20:11:25 +0800

    DP 91

---
+++ dynamic_programming/00091.decode_ways.py
@@ -0,0 +1,31 @@
+
+"""
+Runtime: 32 ms, faster than 81.44% of Python3 online submissions for Decode Ways.
+Memory Usage: 15.6 MB, less than 10.11% of Python3 online submissions for Decode Ways.
+"""
+
+
+class Solution:
+    """
+    count(string) = count(string[1:]) + count(string[2:]) if string[:2] in [10, 26]
+                    count(string[1:]) if string[0] in [2, 9]
+                    0 if string[0] == '0'
+    """
+    @staticmethod
+    def count(string, d):
+        if string == '':
+            return 1
+        elif string[0] == '0':
+            return 0
+        elif string in d:
+            return d[string]
+        elif string[0] == '1' or string[:2] in ('20', '21', '22', '23', '24', '25', '26'):
+            d[string] = Solution.count(string[1:], d) + Solution.count(string[2:], d)
+        else:
+            d[string] = Solution.count(string[1:], d)
+        return d[string]
+
+    def numDecodings(self, s: str) -> int:
+        if s.startswith('0'):
+            return 0
+        return Solution.count(s, {s[-1]: 1})
