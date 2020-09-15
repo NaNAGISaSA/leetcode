0a248b180583d8d42939cdde9174e632b1830096
NaNAGISaSA <cwangzju@126.com>
2020-09-15 09:01:05 +0800

    DP 152

--- dynamic_programming/00152.maximum_product_subarray.py
+++ dynamic_programming/00152.maximum_product_subarray.py
@@ -10,9 +10,12 @@ Memory Usage: 14.1 MB, less than 43.63% of Python3 online submissions for Maximu
 class Solution:
     """
     no dp method
+
+    Ideally, it should cover all cases, and should not reverse the array.
+    But finally, this case can not cover: [-1. -2, -3], so reverse the array and find again.
     """
     def find_max_number(self, array):
-        max_product, neg_product = 1, 1
+        max_product, neg_product = 1, 1  # max_product always positive
         product_list = []
         for index, num in enumerate(array):
             if num < 0:
@@ -47,3 +50,36 @@ class Solution:
         max_number = self.find_max_number(nums)
         max_number_reverse = self.find_max_number(reversed(nums))
         return max(max_number, max_number_reverse)
+
+
+"""
+Runtime: 72 ms, faster than 19.99% of Python3 online submissions for Maximum Product Subarray.
+Memory Usage: 14 MB, less than 63.16% of Python3 online submissions for Maximum Product Subarray.
+"""
+
+
+class Solution2:
+    """
+    no dp method, find the max number directly
+    """
+    def maxProduct(self, nums: List[int]) -> int:
+        import sys
+
+        product = 1
+        max_value = -sys.maxsize - 1
+        for index, number in enumerate(nums):
+            product *= number
+            if product > max_value:
+                max_value = product
+            if product == 0:
+                product = 1
+
+        product = 1
+        for index, number in enumerate(reversed(nums)):
+            product *= number
+            if product > max_value:
+                max_value = product
+            if product == 0:
+                product = 1
+
+        return max_value
