206030dce14e987a6bd99138c3cc4144f05e2c56
NaNAGISaSA <cwangzju@126.com>
2020-09-22 19:05:57 +0800

    DP 213

--- dynamic_programming/00213.house_robber_II.py
+++ dynamic_programming/00213.house_robber_II.py
@@ -9,6 +9,9 @@ Memory Usage: 13.7 MB, less than 89.96% of Python3 online submissions for House 
 
 
 class Solution:
+    """
+    dp[i] = max(dp[i - 1], sort_list + num[0], sort_list + num[i])
+    """
     def count(self, nums: List[int]) -> int:
         for index, num in enumerate(nums[2:], start=2):
             nums[index] = max(nums[index - 1], nums[index - 2] + num)
@@ -24,5 +27,34 @@ class Solution:
             result.append(max(result[i - 1],
                               self.count([0, 0, *nums[1: i - 1]]) + nums[i],
                               self.count([0, 0, *nums[2: i]]) + nums[0]))
-        print(result)
+        return result[-1]
+
+
+"""
+Runtime: 44 ms, faster than 22.63% of Python3 online submissions for House Robber II.
+Memory Usage: 13.8 MB, less than 64.96% of Python3 online submissions for House Robber II.
+"""
+
+
+class Solution2:
+    """
+    for an array [1, 2, 3, 4], can be divided into four cases:
+
+    1) rob first one and last one together -> never happen
+    2) do not rob first one
+    3) do not rob last one
+    4) do not rob first one and last one -> same as 2) and 3) in count method
+    """
+
+    def count(self, nums: List[int]) -> int:
+        for index, num in enumerate(nums[2:], start=2):
+            nums[index] = max(nums[index - 1], nums[index - 2] + num)
+        return nums[-1]
+
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        result = [nums[0]]
+        for i in range(1, len(nums)):
+            result.append(max(self.count([0, 0, *nums[1: i + 1]]), self.count([0, 0, *nums[0: i]])))
         return result[-1]
