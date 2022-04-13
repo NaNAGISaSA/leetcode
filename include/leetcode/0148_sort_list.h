28512550d90c7676a3e6aa8a214bdec4af3c2b36
NaNAGISaSA <cwangzju@126.com>
2022-04-13 08:42:41 +0800

    Finish Sort List With Heap Sort

---
+++ include/leetcode/0148_sort_list.h
@@ -0,0 +1,15 @@
+#ifndef _LEETCODE_SORT_LIST_H_
+#define _LEETCODE_SORT_LIST_H_
+
+#include "utils/linked_list_utils.h"
+
+namespace leetcode::sort_list {
+
+class Solution {
+public:
+    static ListNode* sort_list(ListNode* head);
+};
+
+}  // namespace leetcode::sort_list
+
+#endif
---
+++ src/0148_sort_list/solution.cc
@@ -0,0 +1,30 @@
+#include <memory>
+#include <queue>
+
+#include "0148_sort_list.h"
+
+namespace leetcode::sort_list {
+
+ListNode* Solution::sort_list(ListNode* head) {
+    if (head == nullptr) {
+        return head;
+    }
+    auto cmp = [](ListNode* a, ListNode* b) { return a->val >= b->val; };
+    std::priority_queue<ListNode*, std::vector<ListNode*>, decltype(cmp)> min_heap(cmp);
+    while (head != nullptr) {
+        min_heap.push(head);
+        head = head->next;
+    }
+    auto dummy = std::make_unique<ListNode>(-1);
+    head = dummy.get();
+    while (!min_heap.empty()) {
+        ListNode* top = min_heap.top();
+        min_heap.pop();
+        head->next = top;
+        head = head->next;
+    }
+    head->next = nullptr;
+    return dummy.get()->next;
+}
+
+}  // namespace leetcode::sort_list
