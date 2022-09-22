b81d9da8b692256021d177cb5d3c9ebc29f0bfea
NaNAGISaSA <cwangzju@126.com>
2022-09-22 18:52:51 +0800

    Finish Top K Frequent Elements With Bucket Sort

--- src/0347_top_k_frequent_elements/bucket_sort_solution.cc
+++ src/0347_top_k_frequent_elements/bucket_sort_solution.cc
@@ -9,11 +9,21 @@ std::vector<int> Solution::top_k_frequent(const std::vector<int>& nums, int k) {
     for (const auto& num : nums) {
         ++count_map[num];
     }
-    std::vector<int> bucket(nums.size() + 1, 0);
-
+    std::vector<std::vector<int>> bucket(nums.size() + 1, std::vector<int>{});
+    for (auto it = count_map.begin(), end = count_map.end(); it != end; ++it) {
+        bucket[it->second].push_back(it->first);
+    }
     std::vector<int> result;
     result.reserve(k);
-
+    for (size_t i = bucket.size() - 1; i > 0; --i) {
+        if (!bucket[i].empty()) {
+            result.insert(result.end(), bucket[i].begin(), bucket[i].end());
+            k -= static_cast<int>(bucket[i].size());
+            if (k == 0) {
+                break;
+            }
+        }
+    }
     return result;
 }
 
