300aaeb0256b0e3eb9bf36da3b0224b9e09d5c95
NaNAGISaSA <cwangzju@126.com>
2022-10-09 18:56:16 +0800

    Finish Max Sum Of Rectangle No Larger Than K

--- src/0363_max_sum_of_rectangle_no_larger_than_k/solution.cc
+++ src/0363_max_sum_of_rectangle_no_larger_than_k/solution.cc
@@ -1,8 +1,34 @@
-// #include "0363_max_sum_of_rectangle_no_larger_than_k.h"
+#include "0363_max_sum_of_rectangle_no_larger_than_k.h"
 
-// namespace leetcode::max_sum_of_rectangle_no_larger_than_k {
+#include <algorithm>
+#include <limits>
+#include <set>
 
-// int Solution::max_sum_submatrix(const std::vector<std::vector<int>>& matrix, int k) {
-// }
+namespace leetcode::max_sum_of_rectangle_no_larger_than_k {
 
-// }  // namespace leetcode::max_sum_of_rectangle_no_larger_than_k
+int Solution::max_sum_submatrix(const std::vector<std::vector<int>>& matrix, int k) {
+    size_t m = matrix.size(), n = matrix[0].size();
+    int result = std::numeric_limits<int>::min();
+    for (size_t i = 0; i < m; ++i) {
+        std::vector<int> acc(n, 0);
+        for (size_t j = i; j < m; ++j) {
+            for (size_t k = 0; k < n; ++k) {
+                acc[k] += matrix[j][k];
+            }
+
+            std::set<int> set{0};
+            int acc_tmp = 0;
+            for (auto num : acc) {
+                acc_tmp += num;
+                auto it = set.lower_bound(acc_tmp - k);
+                if (it != set.end()) {
+                    result = std::max(result, acc_tmp - *it);
+                }
+                set.insert(acc_tmp);
+            }
+        }
+    }
+    return result;
+}
+
+}  // namespace leetcode::max_sum_of_rectangle_no_larger_than_k
