77e3d5e16df773130f15dbb21d0838a2b7689b1c
NaNAGISaSA <cwangzju@126.com>
2022-10-17 17:16:46 +0800

    Finish Sum Of Two Integers

--- src/0371_sum_of_two_integers/solution.cc
+++ src/0371_sum_of_two_integers/solution.cc
@@ -1,8 +1,15 @@
-// #include "0371_sum_of_two_integers.h"
+#include "0371_sum_of_two_integers.h"
 
-// namespace leetcode::sum_of_two_integers {
+namespace leetcode::sum_of_two_integers {
 
-// int Solution::get_sum(int a, int b) {
-// }
+int Solution::get_sum(int a, int b) {
+    int carry = 0;
+    while (b) {
+        carry = a & b;
+        a = a ^ b;
+        b = static_cast<unsigned>(carry) << 1;
+    }
+    return a;
+}
 
-// }  // namespace leetcode::sum_of_two_integers
+}  // namespace leetcode::sum_of_two_integers
--- tests/0371_sum_of_two_integers.cc
+++ tests/0371_sum_of_two_integers.cc
@@ -1,10 +1,11 @@
-// #include "0371_sum_of_two_integers.h"
+#include "0371_sum_of_two_integers.h"
 
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-// using namespace leetcode::sum_of_two_integers;
+using namespace leetcode::sum_of_two_integers;
 
-// TEST(SUM_OF_TWO_INTEGERS, EXAMPLE_TEST) {
-//     ASSERT_EQ(Solution::get_sum(3, 19), 22);
-//     ASSERT_EQ(Solution::get_sum(3, -9), -6);
-// }
+TEST(SUM_OF_TWO_INTEGERS, EXAMPLE_TEST) {
+    ASSERT_EQ(Solution::get_sum(3, 19), 22);
+    ASSERT_EQ(Solution::get_sum(3, -9), -6);
+    ASSERT_EQ(Solution::get_sum(1, -1), 0);
+}
